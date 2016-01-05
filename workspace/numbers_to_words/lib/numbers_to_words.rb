f5420130fc35e644c87ac8d5c5be503d1ed0b762
gk and ps <Guest@epicodus-60.local>
2016-01-05 14:40:47 -0800

    major functional changes

--- workspace/numbers_to_words/lib/numbers_to_words.rb
+++ workspace/numbers_to_words/lib/numbers_to_words.rb
@@ -1,31 +1,69 @@
-def numbers_to_words(num)
-  output = ""
-  if num == 0
-    return "zero"
-  end
+class Fixnum
+
   positions = {
     0 => {0 => "", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"},
-    1 => {0 => "", 1 => {1 => "eleven"}, 2 => "twenty", 3 => "thirty", 4 => "forty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety"},
-    2 => "hundred"
+    1 => {0 => "", 1 => {1 => "eleven", 2 => "twelve", 3 => "thirteen", 4 => "fourteen", 5 => "fifteen", 6 => "seventeen", 8 => "eightteen", 9 => "nineteen"},
+    2 => "twenty", 3 => "thirty", 4 => "forty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety"},
+    2 => "hundred",
+    3 => "thousand",
+    4 => "thousand"
   }
-  num = num.to_s.split('')
-  len = num.length - 1
-  range = len..0
-  num.each_with_index { |value,key| num[key] = value.to_i }
-  num.reverse!
-  (range.first).downto(range.last).each do |position|
-    if position == 0
-      return positions[0].fetch(num[-1])
-    elsif position == 1
-      temp = ""
-      temp += " " + positions[position].fetch(num[position]) + " "
-      temp += positions[0].fetch(num[0])
-      output += temp
-      return output.strip
+
+  len = 0
+  position = 0
+
+  define_method(:numbers_to_words) do
+    output = ""
+    if self == 0
+      return "zero"
     end
-    temp = ""
-    temp += " " + positions[0].fetch(num[-1]) + " "
-    temp += positions[position]
-    output += temp
+    num = self.to_s.split('')
+    len = num.length - 1
+    position = len
+    num.each_with_index { |value,key| num[key] = value.to_i }
+    num.reverse!
+    num_array = num.each_slice(3).to_a
+    num_array.reverse!
+    puts num_array
+    num_array.each_with_index do |value, index|
+      if position > 2
+        append = positions[position]
+      else
+        append = ""
+      end
+      group = groups_of_three(num_array[index])
+      output += group + append
+    end
+    return output
+  end
+
+  define_method(:groups_of_three) do |group|
+    pos = group.length - 1
+    output = ""
+    loop do
+      if pos == 0
+        return positions[0].fetch(num[-1])
+      elsif pos == 1
+        temp = ""
+        if group[pos] == 1
+          temp += positions[1][1].fetch(group[pos - 1])
+          output += temp
+          return output
+        end
+        temp += " " + positions[pos].fetch(group[pos]) + " "
+        temp += positions[0].fetch(group[0])
+        temp += output
+        return output
+      end
+
+      temp = ""
+      temp += " " + positions[0].fetch(num[pos]) + " "
+      temp += positions[pos]
+      output += temp
+      position -= group.length
+      pos -= 1
+      break if pos < 1
+    end
+    return output
   end
 end
--- workspace/numbers_to_words/spec/numbers_to_words_spec.rb
+++ workspace/numbers_to_words/spec/numbers_to_words_spec.rb
@@ -1,8 +1,26 @@
 require('rspec')
 require('numbers_to_words')
+require('pry')
 
-describe('numbers_to_words') do
+describe('Fixnum#numbers_to_words') do
   it ('converts a single digit number into a word') do
-    expect(numbers_to_words(120)).to(eq('one'))
+    expect((1).numbers_to_words).to(eq('one'))
+  end
+
+  it ('converts a two digit number into a word') do
+    expect((12).numbers_to_words).to(eq('twelve'))
+  end
+
+  it ('converts a three digit number into a word') do
+    expect((120).numbers_to_words).to(eq('one hundred twenty'))
+  end
+
+  it ('converts a four digit number into a word') do
+    expect((1200).numbers_to_words).to(eq('one thousand two hundred'))
+    expect((9999).numbers_to_words).to(eq('nine thousand nine hundred ninety nine'))
+  end
+
+  it ('converts a five digit number into a word') do
+    expect((12000).numbers_to_words).to(eq('twelve thousand'))
   end
 end
