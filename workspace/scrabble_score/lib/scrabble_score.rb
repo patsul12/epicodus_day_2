f19531183b07af2be586e1dd8f64bd63f5e00ce9
gk and ps <Guest@epicodus-60.local>
2016-01-05 09:31:17 -0800

    Add tests

--- workspace/scrabble_score/lib/scrabble_score.rb
+++ workspace/scrabble_score/lib/scrabble_score.rb
@@ -1,9 +1,14 @@
 class String
   def scrabble_score
     score = 0
-    word = self.split
+    word = self.downcase.split('')
     points = {1 => ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'],
-              2 => ['d','g']}
+              2 => ['d','g'],
+              3 => ['b', 'c', 'm', 'p'],
+              4 => ['f', 'h', 'v', 'w', 'y'],
+              5 => ['k'],
+              8 => ['j', 'x'],
+              10 => ['q', 'z']}
 
     for char in word
       points.each do |key, array|
--- workspace/scrabble_score/spec/scrabble_score_spec.rb
+++ workspace/scrabble_score/spec/scrabble_score_spec.rb
@@ -5,4 +5,40 @@ describe('scrabble_score') do
   it("returns a scrabble score for a letter") do
     expect("a".scrabble_score()).to(eq(1))
   end
+
+  it("returns the sum of a word's points") do
+    expect("aeioulnrst".scrabble_score()).to(eq(10))
+  end
+
+  it("returns the sum of a word's points") do
+    expect("dg".scrabble_score()).to(eq(4))
+  end
+
+  it("returns the sum of a word's points") do
+    expect("bcmp".scrabble_score()).to(eq(12))
+  end
+
+  it("returns the sum of a word's points") do
+    expect("fhvwy".scrabble_score()).to(eq(20))
+  end
+
+  it("returns the sum of a word's points") do
+    expect("k".scrabble_score()).to(eq(5))
+  end
+
+  it("returns the sum of a word's points") do
+    expect("jx".scrabble_score()).to(eq(16))
+  end
+
+  it("returns the sum of a word's points") do
+    expect("qz".scrabble_score()).to(eq(20))
+  end
+
+  it("returns the sum of a word's points") do
+    expect("AQJ".scrabble_score()).to(eq(19))
+  end
+
+  it("returns the sum of a word's points") do
+    expect("scrabble".scrabble_score()).to(eq(14))
+  end
 end
