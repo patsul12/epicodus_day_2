c8f2a6dc04d71664873b59b77e1e3f45726c1f0b
gk and ps <Guest@epicodus-60.local>
2016-01-05 16:49:14 -0800

    added tests

--- workspace/numbers_to_words/lib/numbers_to_words.rb
+++ workspace/numbers_to_words/lib/numbers_to_words.rb
@@ -4,11 +4,19 @@ class Fixnum
     0 => {0 => "", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"},
     1 => {0 => "", 1 => {1 => "eleven", 2 => "twelve", 3 => "thirteen", 4 => "fourteen", 5 => "fifteen", 6 => "seventeen", 8 => "eightteen", 9 => "nineteen"},
       2 => "twenty", 3 => "thirty", 4 => "forty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety"},
-    2 => "hundred",
+    2 => "",
     3 => "thousand",
     4 => "thousand",
     5 => "thousand",
     6 => "million",
+    7 => "million",
+    8 => "million",
+    9 => "billion",
+    10 => "billion",
+    11 => "billion",
+    12 => "trillion",
+    13 => "trillion",
+    14 => "trillion"
   }
 
   len = 0
@@ -27,9 +35,12 @@ class Fixnum
     num.reverse!
     num_array = num.each_slice(3).to_a
     num_array.reverse!
+
     num_array.each_with_index do |value, index|
+      position -= num_array[index].length - 1
+      append = ""
       group = groups_of_three(num_array[index])
-      if position > 1 && num_array[index][2] != 0
+      if position > 2 && num_array[index][2] != 0
         append = positions[position]
       else
         append = ""
@@ -46,7 +57,6 @@ class Fixnum
 
   define_method(:groups_of_three) do |group|
     pos = group.length - 1
-    position -= pos
     output = ""
     loop do
       if pos == 0
@@ -65,8 +75,10 @@ class Fixnum
       elsif pos == 2
         if group == [0,0,0]
           output += ""
-        else
+        elsif group[2] > 0
           output += positions[0].fetch(group[pos]) + " hundred"
+        else
+          output += positions[0].fetch(group[pos])
         end
       end
       pos -= 1
--- workspace/numbers_to_words/spec/numbers_to_words_spec.rb
+++ workspace/numbers_to_words/spec/numbers_to_words_spec.rb
@@ -17,7 +17,6 @@ describe('Fixnum#numbers_to_words') do
   end
 
   it ('converts a four digit number into a word') do
-
     expect((9999).numbers_to_words).to(eq('nine thousand nine hundred ninety nine'))
   end
 
@@ -29,11 +28,37 @@ describe('Fixnum#numbers_to_words') do
     expect((120000).numbers_to_words).to(eq('one hundred twenty thousand'))
   end
 
-  it ('converts a six digit number into a word') do
+  it ('converts a seven digit number into a word') do
     expect((1200000).numbers_to_words).to(eq('one million two hundred thousand'))
+    expect((9999999).numbers_to_words).to(eq('nine million nine hundred ninety nine thousand nine hundred ninety nine'))
   end
 
-  it ('converts a six digit number into a word') do
+  it ('converts a eight digit number into a word') do
     expect((12000000).numbers_to_words).to(eq('twelve million'))
+    expect((99999999).numbers_to_words).to(eq('ninety nine million nine hundred ninety nine thousand nine hundred ninety nine'))
+  end
+
+  it ('converts a nine digit number into a word') do
+    expect((120000000).numbers_to_words).to(eq('one hundred twenty million'))
+    expect((999999999).numbers_to_words).to(eq('nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine'))
+  end
+
+  it ('converts a ten digit number into a word') do
+    expect((1200000000).numbers_to_words).to(eq('one billion two hundred million'))
+    expect((9999999999).numbers_to_words).to(eq('nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine'))
+  end
+
+  it ('converts a eleven digit number into a word') do
+    expect((12000000000).numbers_to_words).to(eq('twelve billion'))
+  end
+
+  it ('converts a twelve digit number into a word') do
+    expect((120000000000).numbers_to_words).to(eq('one hundred twenty billion'))
+    expect((999999999999).numbers_to_words).to(eq('nine hundred ninety nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine'))
+  end
+
+  it ('converts a thirteen digit number into a word') do
+    expect((1200000000001).numbers_to_words).to(eq('one trillion two hundred billion one'))
+    expect((9_999_999_999_999).numbers_to_words).to(eq('nine trillion nine hundred ninety nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine'))
   end
 end
